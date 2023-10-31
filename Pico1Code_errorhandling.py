4ba517d6ef82fb066c7b045b4d4c61b61c7eb3fe
AaraniG <145384476+AaraniG@users.noreply.github.com>
2023-10-31 11:17:39 -0400

    Update Pico1Code_errorhandling.py

--- Pico1Code_errorhandling.py
+++ Pico1Code_errorhandling.py
@@ -1,8 +1,11 @@
+'''
+ChatGPT was used for guidance in adding error handling
+'''
 from machine import PWM, UART, Pin
 import time
 
-# Constant for PWM
-PWM_PIN = Pin(2)  # Set to the GPIO Pin number (e.g., GPIO2)
+# Constants for PWM
+PWM_PIN = Pin(2)  # Set to the GPIO Pin number
 PWM_FREQ = 1000   # Desired PWM frequency
 
 # Initialize PWM
@@ -17,13 +20,17 @@ while True:
     try:
         desired_pwm_value = float(input("Enter desired PWM Value (0-100%): "))
         if 0 <= desired_pwm_value <= 100:
+            # Calculate the duty cycle for the PWM signal based on the desired PWM value
             pwm.duty_u16(int((desired_pwm_value / 100) * 65535))
+            # Send the desired PWM value via UART
             uart.write("Desired PWM Value: {:.2f}%\n".format(desired_pwm_value))
         else:
             print("Error: Desired PWM value should be between 0 and 100%")
     except ValueError:
+        # Handle the case where the user enters invalid input
         print("Error: Invalid input. Please enter a valid number.")
     except KeyboardInterrupt:
+        # Handle the case where the user presses Ctrl+C to exit the program
         print("Exiting the program.")
         break
 
